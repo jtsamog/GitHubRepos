d3113d14efc0a54c304f67d6da676021481a78e1
Sam Ogunyowa <mailsamog@gmail.com>
2017-07-17 15:45:37 -0400

    initial commit

---
+++ GitHubRepos/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  GitHubRepos
+//
+//  Created by Endeavour2 on 7/17/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
