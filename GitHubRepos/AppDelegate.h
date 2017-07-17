d6326936595f27cb07f52ce6a38131b993d1353a
Sam Ogunyowa <mailsamog@gmail.com>
2017-07-17 15:26:00 -0400

    Initial Commit

---
+++ GitHubRepos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubRepos
+//
+//  Created by Endeavour2 on 7/17/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
